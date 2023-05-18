0a90ece6e9458df60e4bed05cffdb3e37d66e83a
Mehedi1324 <you@example.com>
2023-05-18 09:46:09 +0600

    new updates

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 async function run() {
   try {
